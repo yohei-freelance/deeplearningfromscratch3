ff43f8fd202cde90173e922b61e6899503485610
yohei-freelance <n-yohei-1717@g.ecc.u-tokyo.ac.jp>
2020-07-04 23:20:54 +0900

    commit

---
+++ steps/step1.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+class Variable:
+  def __init__(self, data):
+    self.data = data
+
+if __name__ == '__main__':
+  data = np.array(1.0)
+  x = Variable(data)
+  print(x.data)
---
+++ steps/step2.py
@@ -0,0 +1,24 @@
+import numpy as np
+from step1 import Variable
+
+class Function:
+  def __call__(self, input):
+    x = input.data
+    y = self.forward(x)
+    output = Variable(y)
+    return output
+
+  def forward(self, x):
+    raise NotImplementedError()
+
+class Square(Function):
+  def forward(self, x):
+    return x ** 2
+
+if __name__ == '__main__':
+  x = Variable(np.array(10))
+  f = Square()
+  y = f(x)
+
+  print(type(y))
+  print(y.data)
